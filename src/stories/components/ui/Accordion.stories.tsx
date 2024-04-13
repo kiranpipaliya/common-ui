2f7778b5c6b0f823382993b19ded4d123e65fa77
kiranpipaliya <kiranpipaliya446@gmail.com>
2024-04-13 19:27:42 +0530

    added accordion for pr test

---
+++ src/stories/components/ui/Accordion.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta } from '@storybook/react';
+import React from 'react';
+
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from 'components/ui/accordion';
+
+// Import your components
+
+export default {
+	title: 'Accordion',
+	component: Accordion,
+	tags: ['autodocs'],
+} as Meta;
+
+// Story for the Accordion component
+export const DefaultAccordion = () => (
+	<Accordion type="single" collapsible className="w-1/2">
+		<AccordionItem value="value1">
+			<AccordionTrigger>First Accordion Item</AccordionTrigger>
+			<AccordionContent>Content of the first Accordion Item</AccordionContent>
+		</AccordionItem>
+	</Accordion>
+);
